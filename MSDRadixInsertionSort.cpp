65ea2388f886ed4e365e52d7fc7e965950b35c0b
Mrugank <mrugank2@gmail.com>
2021-02-22 21:06:55 -0500

    finished verison

--- MSDRadixInsertionSort.cpp
+++ MSDRadixInsertionSort.cpp
@@ -22,12 +22,14 @@ using namespace std;
 // helper function to generate a random array //
 //--------------------------------------------//
 
+// Node for the linked list in BucketSort
 struct Node {
     unsigned long int val;
     Node * next;
     Node(unsigned long int val, Node * node) : val{val}, next{node} {}
 };
 
+// Calculates a^n
 unsigned long int pow (unsigned long int a, unsigned long int n) {
     if (n == 0) {
         return 1;
@@ -42,6 +44,7 @@ unsigned long int pow (unsigned long int a, unsigned long int n) {
     }
 }
 
+// Finds the nth digit of a number x
 int nthDigit(unsigned long int x, int n) {
 
     unsigned long int a = x / pow(10, 10 - n - 1);
@@ -49,25 +52,24 @@ int nthDigit(unsigned long int x, int n) {
     return a % 10;
 }
 
-void printArray(vector<unsigned long int> &A) {
-    int len = A.size();
-    for (int i = 0; i < len; i++) {
-        cout << A[i] << ", ";
-    }
-    cout << endl << endl;
-}
 
 void bucketSort(vector<unsigned long int> &A, int l, int r, int d) {
 
+    // Creates a vector of pairs
+    // pair.first represents the linked list, pair.second represents the last element of the linked list
     vector<std::pair<Node *, Node *>> B = vector<std::pair<Node *, Node *>>(10, std::pair<Node *, Node *>(nullptr, nullptr));
 
     for (int i = l; i <= r; i++) {
+        // create a new element node
         Node * elm = new Node(A[i], nullptr);
         int dthDigit = nthDigit(A[i], d);
+
+        // place it in the dth digit bucket, if an element exists, append it to the end of the linked list there
         if (B[dthDigit].first != nullptr) {
             B[dthDigit].second->next = elm;
             B[dthDigit].second = elm;
         }
+        // otherwise create a new list at the index
         else {
             B[dthDigit].first = elm;
             B[dthDigit].second = elm;
@@ -76,9 +78,12 @@ void bucketSort(vector<unsigned long int> &A, int l, int r, int d) {
 
     int index = l;
     for (int i = 0; i < 10; i++) {
+        // while there exists a linked list
         while (B[i].first != nullptr) {
             auto tmp = B[i].first;
+            // add the head of the list to the indexed position in the original Array
             A[index++] = tmp->val;
+            // remove the current head and make the next element the new head in the list
             B[i].first = tmp->next;
             delete tmp;
         }
@@ -124,11 +129,13 @@ void MSDSort(vector<unsigned long int> &A, int m, int l, int r, int d, int thres
         int l_Prime = l;
         while (l_Prime < r) {
             int dthDigit = nthDigit(A[l_Prime], d);
+            // get the start and end of the subarray of elements that have the same dth digit
             int r_Prime = l_Prime;
             while (dthDigit == nthDigit(A[r_Prime], d)) {
                 r_Prime++;
             }
             r_Prime--;
+            // Run MSDSort on that subarray of elements
             MSDSort(A, m, l_Prime, r_Prime, d + 1, threshold, total_num_calls);
             l_Prime = r_Prime + 1;
         }
@@ -149,13 +156,12 @@ void getRandomArray(vector<unsigned long int>& A)
 int main()
 {
     int m = 10;
-    int threshold = 100;
+    int threshold = 2;
     int n = 100000;
-    // int n = 1000;
     vector<unsigned long int> A;
 
     // for tests on a random array, set this variable to false
-    bool read_input_from_cin = false;
+    bool read_input_from_cin = true;
     
     if (read_input_from_cin)
     {
@@ -186,19 +192,18 @@ int main()
     
     bool sorted = true;
     // print re-arranged A  to standard output    
-    // cout << A[0] << endl;
-    // for (int i = 1; i < n; i++)
-    // {
-    //     cout << A[i] << endl;
-    //     if (A[i] < A[i - 1])
-    //         sorted = false;       
-    // }
+    cout << A[0] << endl;
+    for (int i = 1; i < n; i++)
+    {
+        cout << A[i] << endl;
+        if (A[i] < A[i - 1])
+            sorted = false;       
+    }
     
      cout << total_num_calls << endl;
         
     // uncomment this line to have running time printed to the standard output
-    cout << "Time to sort: " <<  chrono::duration <double, milli>(diff).count() << " ms" << endl;
-    cout << n << endl;
+    // cout << "Time to sort: " <<  chrono::duration <double, milli>(diff).count() << " ms" << endl;
 
     return 0;
 
